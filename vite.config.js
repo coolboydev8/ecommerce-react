4ab5d7e74271a4d90583e1d037d14c27527e5e49
coolboydev8 <goldenhouse0601@gmail.com>
2023-02-21 21:21:42 +0100

    fix urls

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/E-commerce-react-app/",
+  base: "/e-commerce-react-app/",
   plugins: [react()]
 })
