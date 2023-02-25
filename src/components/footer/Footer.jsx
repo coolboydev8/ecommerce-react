d591bd86d80fb72d41fd705ea5fc538b6bbe93bd
coolboydev8 <goldenhouse0601@gmail.com>
2023-02-25 12:18:28 +0100

    add Footer component with styles

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FooterContainer } from './FooterStyles'
+
+export const Footer = () => {
+  return (
+    <FooterContainer>
+        Footer
+    </FooterContainer>
+  )
+}
+
