d553fc63a48a7fab64e125530a3fdb8ffe407bcd
coolboydev8 <goldenhouse0601@gmail.com>
2023-02-25 16:38:30 +0100

    styling header

--- src/components/header/HeaderStyles.jsx
+++ src/components/header/HeaderStyles.jsx
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 export const HeaderContainer = styled.div`
     width: 100vw;
     box-shadow: 0 13px 18px -14px #888888;
-    background: linear-gradient(to right, #F67062, #FC5296);
     background: linear-gradient(to right, #FF0000, #990000);
 `;
 
