8e135a3434694a6f3556e8b17cca7b177160d23a
coolboydev8 <goldenhouse0601@gmail.com>
2023-03-11 17:12:31 +0100

    display cart items

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../../context/ShopContext'
+import { ITEMS } from '../../data/items';
+import { Item } from '../shop/Item';
 
 export const Cart = () => {
+  const { cartItems } = useContext(ShopContext);
+
   return (
-    <div className='wrapper'>Cart</div>
+    <div className='wrapper'>
+      <div>
+        <h1>Cart Items:</h1>
+      </div>
+      <div>
+        {ITEMS.map((item) => {
+            if (cartItems[item.id] !== 0 ) {
+              return <Item key={item.id} {...item} />
+            }
+        })}
+      </div>
+    </div>
   )
 }
