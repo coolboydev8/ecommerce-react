2a613d0e4b3f02fe876bc3c43e531f5feca5d148
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-21 17:44:28 +0200

    delete pointer from Item box

--- src/pages/item/ItemStyles.jsx
+++ src/pages/item/ItemStyles.jsx
@@ -7,7 +7,6 @@ export const ItemContainer = styled.div`
   justify-content: center;
   width: 200px;
   margin: 16px;
-  cursor: pointer;
   &:hover {
     box-shadow: 0px 0px 24px -5px rgba(66, 68, 90, 1);
     -webkit-box-shadow: 0px 0px 24px -5px rgba(66, 68, 90, 1);
