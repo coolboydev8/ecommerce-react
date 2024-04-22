e249ef618a777e6f56f448b2fc8a353934143324
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-22 12:46:59 +0200

    add new objects - ps5 games

---
+++ src/data/games.js
@@ -0,0 +1,59 @@
+import ps5game01 from './../assets/img/games/ps5/ps5_game_01.jpg'
+import ps5game02 from './../assets/img/games/ps5/ps5_game_02.jpg'
+import ps5game03 from './../assets/img/games/ps5/ps5_game_03.jpg'
+import ps5game04 from './../assets/img/games/ps5/ps5_game_04.jpg'
+import ps5game05 from './../assets/img/games/ps5/ps5_game_05.jpg'
+import ps5game06 from './../assets/img/games/ps5/ps5_game_06.jpg'
+import ps5game07 from './../assets/img/games/ps5/ps5_game_07.jpg'
+import ps5game08 from './../assets/img/games/ps5/ps5_game_08.jpg'
+
+export const PS5GAMES = [
+    {
+        id: 1,
+        itemName: "Game Sample Name",
+        price: 145.00,
+        itemImage: ps5game01,
+    },
+    {
+        id: 2,
+        itemName: "Game Sample Name",
+        price: 140.00,
+        itemImage: ps5game02,
+    },
+    {
+        id: 3,
+        itemName: "Game Sample Name",
+        price: 140.00,
+        itemImage: ps5game03,
+    },
+    {
+        id: 4,
+        itemName: "Game Sample Name",
+        price: 155.00,
+        itemImage: ps5game04,
+    },
+    {
+        id: 5,
+        itemName: "Game Sample Name",
+        price: 154.99,
+        itemImage: ps5game05,
+    },
+    {
+        id: 6,
+        itemName: "Game Sample Name",
+        price: 148.99,
+        itemImage: ps5game06,
+    },
+    {
+        id: 7,
+        itemName: "Game Sample Name",
+        price: 199.99,
+        itemImage: ps5game07,
+    },
+    {
+        id: 8,
+        itemName: "Game Sample Name",
+        price: 149.50,
+        itemImage: ps5game08,
+    },
+]
