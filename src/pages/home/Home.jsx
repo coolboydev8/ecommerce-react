26f1c6d0d9764bb5f21f651e784ca177b8ef6b5e
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-23 08:30:12 +0200

    add SwiperHero component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import SwiperHero from "./SwiperHero";
 
 export const Home = () => {
   return (
     <div className="wrapper shop">
       <h1>Home Page</h1>
+      <SwiperHero />
     </div>
   );
 };
