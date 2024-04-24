642fd34d627887c7b6c00b986c5a0ad83419f8af
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-24 11:15:46 +0200

    styling SEO texts

---
+++ src/pages/home/top5/SeoTextStyles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const SeoTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  max-width: 1100px;
+  ol {
+    li {
+      margin: 0 0 4px 0;
+    }
+  }
+`;
