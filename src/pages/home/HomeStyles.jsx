1c973e9249680e45778fdd5b9976064e0ef10599
coolboydev8 <goldenhouse0601@gmail.com>
2024-04-24 16:46:20 +0200

    styling mobile seo texts, titles

--- src/pages/home/HomeStyles.jsx
+++ src/pages/home/HomeStyles.jsx
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const Title = styled.h1`
   display: flex;
   justify-content: center;
+  font-size: 30px;
+  @media (max-width: 800px) {
+    font-size: 26px;
+  }
 `;
--- src/pages/home/top5/SeoTextStyles.jsx
+++ src/pages/home/top5/SeoTextStyles.jsx
@@ -5,9 +5,27 @@ export const SeoTextContainer = styled.div`
   flex-direction: column;
   margin: 0 auto;
   max-width: 1100px;
+  p {
+    font-size: 16px;
+    ol {
+      margin: 0;
+    }
+  }
   ol {
+    margin: 0;
     li {
       margin: 0 0 4px 0;
+    }
+  }
+  @media (max-width: 800px) {
+    p {
+      font-size: 14px;
+    }
+    ol {
+      padding-left: 16px;
+      li {
+        font-size: 14px;
+      }
     }
   }
 `;
