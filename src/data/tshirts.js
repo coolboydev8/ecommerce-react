f7735f0e7f5e101bff73fe88d92d65050216bf74
coolboydev8 <goldenhouse0601@gmail.com>
2024-05-05 10:04:11 +0200

    add tshirts data

---
+++ src/data/tshirts.js
@@ -0,0 +1,73 @@
+import tshirt01 from './../assets/img/tshirts/tshirt01.jpg'
+import tshirt02 from './../assets/img/tshirts/tshirt02.jpg'
+import tshirt03 from './../assets/img/tshirts/tshirt03.jpg'
+import tshirt04 from './../assets/img/tshirts/tshirt04.jpg'
+import tshirt05 from './../assets/img/tshirts/tshirt05.jpg'
+import tshirt06 from './../assets/img/tshirts/tshirt06.jpg'
+import tshirt07 from './../assets/img/tshirts/tshirt07.jpg'
+import tshirt08 from './../assets/img/tshirts/tshirt08.jpg'
+import tshirt09 from './../assets/img/tshirts/tshirt09.jpg'
+import tshirt10 from './../assets/img/tshirts/tshirt10.jpg'
+
+export const TSHIRTS = [
+    {
+        id: 301,
+        itemName: "T-shirt",
+        price: 15.00,
+        itemImage: tshirt01,
+    },
+    {
+        id: 302,
+        itemName: "T-shirt",
+        price: 10.00,
+        itemImage: tshirt02,
+    },
+    {
+        id: 303,
+        itemName: "T-shirt",
+        price: 10.00,
+        itemImage: tshirt03,
+    },
+    {
+        id: 304,
+        itemName: "T-shirt",
+        price: 15.00,
+        itemImage: tshirt04,
+    },
+    {
+        id: 305,
+        itemName: "T-shirt",
+        price: 14.99,
+        itemImage: tshirt05,
+    },
+    {
+        id: 306,
+        itemName: "T-shirt",
+        price: 18.99,
+        itemImage: tshirt06,
+    },
+    {
+        id: 307,
+        itemName: "T-shirt",
+        price: 9.99,
+        itemImage: tshirt07,
+    },
+    {
+        id: 308,
+        itemName: "T-shirt",
+        price: 9.50,
+        itemImage: tshirt08,
+    },
+    {
+        id: 309,
+        itemName: "T-shirt",
+        price: 9.50,
+        itemImage: tshirt09,
+    },
+    {
+        id: 310,
+        itemName: "T-shirt",
+        price: 9.99,
+        itemImage: tshirt10,
+    },
+]
